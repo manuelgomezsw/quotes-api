7852efeff170a6f120a5d6c6366d7ac2bab437fa
Jose Gómez <manuel.gomez@mercadolibre.com.co>
2021-10-04 22:20:42 -0500

    Added skeleton project.

---
+++ src/main/java/com/quotes/api/entities/Quote.java
@@ -0,0 +1,61 @@
+package com.quotes.api.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Document(collection = "quotes")
+public class Quote {
+    @Id
+    private String id;
+    private String author;
+    private String work;
+    private String message;
+    private String[] tags;
+    private Date dateCreated;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getId() { return id; }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getWork() {
+        return work;
+    }
+
+    public void setWork(String work) {
+        this.work = work;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String[] getTags() {
+        return tags;
+    }
+
+    public void setTags(String[] tags) {
+        this.tags = tags;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated() {
+        this.dateCreated = Date.from(LocalDate.now().atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+    }
+}
